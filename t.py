6b06b07cc24c0a966abe5651bea050cb254bcfc9
caorenxxy <caorenxxy@gmail.com>
2018-02-09 12:55:50 +0800

    modify picture

--- t.py
+++ t.py
@@ -19,8 +19,8 @@ def drawplot():
 	plt.xlabel('x')
 	plt.ylabel('Probablity density')
 	plt.legend() 
-	plt.plot([0, 6], [0.025, 0.025], 'r--')
-	plt.plot([0, 6], [0.2, 0.2], 'r--')
+	plt.plot([0, 6], [0.025, 0.025], 'r--', linewidth = 0.7)
+	plt.plot([0, 6], [0.2, 0.2], 'r--', linewidth = 0.7)
 	tx1 = 0.5
 	ty1 = 0.2
 	plt.text(tx1, ty1, "$q_{ij}$",fontdict=None, withdash=False,fontsize=13)
